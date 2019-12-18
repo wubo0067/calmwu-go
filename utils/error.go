39f1108a133d990e023a5f153b619bb3d2faf969
littlebull <calm.wu@qq.com>
2019-12-18 11:58:00 +0800

    提交，按staticcheck修改

--- utils/error.go
+++ utils/error.go
@@ -6,48 +6,35 @@
  * @Comment:
  */
 
+// Package utils for golang tools functions
 package utils
 
 import (
-	"errors"
-	"fmt"
+	"reflect"
 
-	"github.com/mozhata/merr"
+	"github.com/pkg/errors"
 )
 
+// NewError 构建错误对象
 func NewError(args ...interface{}) error {
 	var err error
 	var rawData []interface{}
 	for _, arg := range args {
-		switch arg.(type) {
+		switch arg := arg.(type) {
 		case error:
 			err = arg.(error)
-			ZLog.Errorf("error", err)
 			continue
 		default:
 			rawData = append(rawData, arg)
 		}
 	}
 	if err == nil {
-		err = errors.New(fmt.Sprintf("%v", rawData))
+		err = errors.Errorf("%v", rawData)
 	}
-	return errors.New(fmt.Sprintf("%v [error => %s]", rawData, err.Error()))
+	return errors.Errorf("%v [error => %s]", rawData, err.Error())
 }
 
-/*
-use example
-
-func GetLocationFor(u *User) (*Location,error){
-  respMsg, err := grpcClient.GetLocationFor(u.name)
-  if err != nil{
-    // here we directly send the error
-    return nil, errors.New("while getting location from grpc client in GetLocationFor", err)
-  }
-  // process the respMsg and move on
-}
-*/
-
-func StrError(err error) string {
-	e := merr.WrapErr(err)
-	return fmt.Sprintf("err: %s\nreason: %s\ncall stack: %s\n", e.Error(), e.RawErr(), e.CallStack())
+// IsInterfaceNil 判断接口是否为空接口
+func IsInterfaceNil(v interface{}) bool {
+	return v == nil || (reflect.ValueOf(v).Kind() == reflect.Ptr && reflect.ValueOf(v).IsNil())
 }
