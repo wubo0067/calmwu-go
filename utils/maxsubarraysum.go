a6d83bb5ba20351bc7282b3c2a3ccc24366bfec1
calmwu <wubo0067@hotmail.com>
2020-06-07 21:42:03 +0800

    优化

--- utils/maxsubarraysum.go
+++ utils/maxsubarraysum.go
@@ -2,7 +2,7 @@
  * @Author: calmwu
  * @Date: 2020-06-07 21:31:04
  * @Last Modified by: calmwu
- * @Last Modified time: 2020-06-07 21:31:55
+ * @Last Modified time: 2020-06-07 21:41:28
  */
 
 package utils
@@ -16,8 +16,8 @@ func Max(x int, y int) int {
 
 // MaxSubarraySum 输出连续子数组最大合
 func MaxSubarraySum(array []int) int {
-	var currentMax int = 0
-	var maxTillNow int = 0
+	currentMax := 0
+	maxTillNow := 0
 	for _, v := range array {
 		currentMax = Max(v, currentMax+v)
 		maxTillNow = Max(maxTillNow, currentMax)
