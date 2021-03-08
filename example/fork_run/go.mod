847176ecbcf90912251b7974469a78b10ecc4681
calmwu <wubo0067@hotmail.com>
2021-03-08 18:07:19 +0800

    fork exec

--- example/fork_run/go.mod
+++ example/fork_run/go.mod
@@ -2,4 +2,7 @@ module fork_run
 
 go 1.16
 
-require github.com/urfave/cli v1.22.5
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/urfave/cli v1.22.5
+)
--- example/fork_run/main.go
+++ example/fork_run/main.go
@@ -8,9 +8,14 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
+	"syscall"
 
+	"github.com/pkg/errors"
 	"github.com/urfave/cli"
 )
 
@@ -18,7 +23,13 @@ const usage = "forkRun run cmd"
 
 var runCmd = cli.Command{
 	Name:  "run",
-	Usage: `Create a child process to run cmd`,
+	Usage: `Create a child process to exec cmd`,
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "ti",
+			Usage: "enable tty",
+		},
+	}, // 子命令选项不算args
 	Action: func(context *cli.Context) error {
 		// 这个是子命令的参数
 		log.Printf("context Args: %#v\n", context.Args())
@@ -28,13 +39,99 @@ var runCmd = cli.Command{
 			cmdArgs[i] = arg
 		}
 
-		runChild(cmdArgs)
+		ForkExec(cmdArgs, context.Bool("ti"))
 		return nil
 	},
 }
 
-func runChild(cmdArgs []string) {
-	log.Printf("cmdArgs: %#v\n", cmdArgs)
+var initCmd = cli.Command{
+	Name:  "init",
+	Usage: "Init child process",
+	Action: func(context *cli.Context) error {
+		// 读取执行的命令
+		pipe := os.NewFile(uintptr(3), "pipe")
+		defer pipe.Close()
+		msg, err := ioutil.ReadAll(pipe)
+		if err != nil {
+			log.Printf("init read pipe failed. err:%s\n", err.Error())
+			return err
+		}
+
+		if len(msg) == 0 {
+			err = errors.New("read empty from pipe")
+			log.Printf("%s\n", err.Error())
+			return err
+		}
+
+		cmdArgs := strings.Split(string(msg), " ")
+
+		// 第一个是执行程序
+		initCmd, err := exec.LookPath(cmdArgs[0])
+		if err != nil {
+			log.Printf("exec LookPath failed. err: %s\n", err.Error())
+			return err
+		}
+
+		log.Printf("find init cmd: %s\n", initCmd)
+		// execv
+		if err := syscall.Exec(initCmd, cmdArgs[0:], []string{}); err != nil {
+			log.Fatalf(err.Error())
+		}
+
+		return nil
+	},
+}
+
+// ForkExec make a duplicate of the current process, execv replaces the
+// duplicate parent process with a new process
+func ForkExec(cmdArgs []string, tty bool) error {
+	log.Printf("cmdArgs: %#v, tty: %v\n", cmdArgs, tty)
+
+	// 子进程命令读取管道
+	rPipe, wPipe, err := os.Pipe()
+	if err != nil {
+		log.Printf("make Pipe failed. err:%s", err.Error())
+		return err
+	}
+
+	// 自身的执行命令
+	selfExe, _ := os.Readlink("/proc/self/exe")
+	log.Printf("selfCmd: %s\n", selfExe)
+
+	cmd := &exec.Cmd{
+		Path: selfExe,
+		Args: []string{selfExe, "init"},
+		Dir: func() string {
+			cwd, _ := os.Getwd()
+			return cwd
+		}(),
+		Stdin:      os.Stdin,
+		Stdout:     os.Stdout,
+		Stderr:     os.Stderr,
+		ExtraFiles: []*os.File{rPipe},
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Printf("cmd Start failed. err: %s\n", err.Error())
+		return err
+	}
+
+	log.Printf("child pid: %d running\n", cmd.Process.Pid)
+
+	// 将exec的命令用管道发送
+	childExe := strings.Join(cmdArgs, " ")
+	wPipe.WriteString(childExe)
+	// 关闭管道
+	wPipe.Close()
+	if tty {
+		// 等待子进程退出
+		log.Printf("tty for wait child\n")
+		cmd.Wait()
+		log.Printf("pid:%d child process exit!\n", cmd.Process.Pid)
+	} else {
+		cmd.Process.Release()
+	}
+	return nil
 }
 
 func main() {
@@ -44,6 +141,7 @@ func main() {
 
 	app.Commands = []cli.Command{
 		runCmd,
+		initCmd,
 	}
 
 	if err := app.Run(os.Args); err != nil {
