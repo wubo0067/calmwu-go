67d02921922d87d349c2d362d3cad939a23ddf1b
calmwu <wubo0067@hotmail.com>
2021-03-09 16:43:28 +0800

    support bash -c 'echo 11212'

--- example/fork_run/main.go
+++ example/fork_run/main.go
@@ -1,3 +1,5 @@
+// +build linux
+
 /*
  * @Author: CALM.WU
  * @Date: 2021-03-07 21:40:47
@@ -63,8 +65,13 @@ var initCmd = cli.Command{
 			return err
 		}
 
-		cmdArgs := strings.Split(string(msg), " ")
-		log.Printf("cmdArgs: %#v\n", cmdArgs)
+		quoted := false
+		cmdArgs := strings.FieldsFunc(string(msg), func(r rune) bool {
+			if r == '\'' {
+				quoted = !quoted
+			}
+			return !quoted && r == ' '
+		})
 
 		// 第一个是执行程序
 		initCmd, err := exec.LookPath(cmdArgs[0])
@@ -73,12 +80,19 @@ var initCmd = cli.Command{
 			return err
 		}
 
-		log.Printf("find init cmd: %s\n", initCmd)
-		// execv
-		if err := syscall.Exec(initCmd, cmdArgs[0:], []string{}); err != nil {
+		// 对args判断 bash -c 'echo 1212' ===> arg[2] = "echo 1212"，不能有单引号
+		for i, cmdArg := range cmdArgs[1:] {
+			if -1 != strings.IndexByte(cmdArg, ' ') {
+				cmdArgs[i+1] = strings.Trim(cmdArg, "'")
+			}
+		}
+		//cmdArgs[2] = strings.Trim(cmdArgs[2], "'")
+		log.Printf("init cmd: {%s}, cmdArgs: %#v\n", initCmd, cmdArgs)
+
+		// execv，已经替换了执行文件，这是最后一条指令，后面都不会执行
+		if err := syscall.Exec(initCmd, cmdArgs, os.Environ()); err != nil {
 			log.Fatalf(err.Error())
 		}
-		log.Printf("--------init exit--------\n")
 		return nil
 	},
 }
@@ -112,15 +126,34 @@ func ForkExec(cmdArgs []string, tty bool) error {
 		ExtraFiles: []*os.File{rPipe},
 	}
 
+	if !tty {
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Foreground: false,
+		}
+	}
+
 	if err := cmd.Start(); err != nil {
 		log.Printf("cmd Start failed. err: %s\n", err.Error())
 		return err
 	}
 
-	log.Printf("child pid: %d running\n", cmd.Process.Pid)
-
 	// 将exec的命令用管道发送
-	childExe := strings.Join(cmdArgs, " ")
+	var cmdStrBuilder strings.Builder
+	for _, arg := range cmdArgs {
+		if -1 != strings.IndexByte(arg, ' ') {
+			// 这个string中存在空格，需要作为一个整体参数，用单引号括起来
+			cmdStrBuilder.WriteByte('\'')
+			cmdStrBuilder.WriteString(arg)
+			cmdStrBuilder.WriteByte('\'')
+		} else {
+			cmdStrBuilder.WriteString(arg)
+		}
+		cmdStrBuilder.WriteByte(' ')
+	}
+
+	childExe := strings.TrimSpace(cmdStrBuilder.String())
+	log.Printf("child pid: %d running, child exe cmd: {%s}\n", cmd.Process.Pid, childExe)
+
 	wPipe.WriteString(childExe)
 	// 关闭管道
 	wPipe.Close()
