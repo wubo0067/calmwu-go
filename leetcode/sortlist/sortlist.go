356ebfe19612948f2b623ce580a3ebf93d7ebe0b
calmwu <wubo0067@hotmail.com>
2021-03-21 17:21:52 +0800

    leetcode: 链表排序

---
+++ leetcode/sortlist/sortlist.go
@@ -0,0 +1,16 @@
+/*
+ * @Author: calmwu
+ * @Date: 2021-03-21 17:20:49
+ * @Last Modified by: calmwu
+ * @Last Modified time: 2021-03-21 17:21:22
+ */
+
+package main
+
+// ListNode single list node
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {}
