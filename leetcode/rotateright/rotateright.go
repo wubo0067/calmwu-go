b04e278bb61227da5548ddb859503f1d41cc1623
calmwu <wubo0067@hotmail.com>
2021-03-30 16:28:46 +0800

    beat double 100%

--- leetcode/rotateright/rotateright.go
+++ leetcode/rotateright/rotateright.go
@@ -7,32 +7,98 @@
 
 package main
 
+import "fmt"
+
 // ListNode single list node
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
-func searchTailAndPrevNode(l *ListNode) (tailNode, tailPrevNode *ListNode) {
-	tailNode = nil
-	tailPrevNode = nil
-	return
+func listLength(l *ListNode) int {
+	len := 1
+	for l.Next != nil {
+		len++
+		l = l.Next
+	}
+	return len
+}
+
+// searchTailAndPrevNode return tailNode, tailPrevNode
+func searchTailAndPrevNode(l *ListNode) (*ListNode, *ListNode) {
+	tailPrevNode := l
+
+	for {
+		if tailPrevNode.Next.Next == nil {
+			return tailPrevNode.Next, tailPrevNode
+		}
+		tailPrevNode = tailPrevNode.Next
+	}
 }
 
 func rotateRight(head *ListNode, k int) *ListNode {
-	if k == 0 {
+	if k == 0 || head == nil || head.Next == nil {
 		return head
 	}
 
-	headNode := head
+	//headNode := head
+
+	len := listLength(head)
+	k = k % len
+	//fmt.Printf("k: %d, len: %d\n", k, len)
 
 	for k > 0 {
+		tailNode, tailPrevNode := searchTailAndPrevNode(head)
+		if tailPrevNode == nil {
+			return tailNode
+		}
+		tailPrevNode.Next = nil
+		tailNode.Next = head
+		head = tailNode
 		k--
 	}
 
-	return headNode
+	return head
+}
+
+func printListNodes(l *ListNode) {
+	i := 0
+
+	for l != nil {
+		fmt.Printf("res[%d] = %d\n", i, l.Val)
+		l = l.Next
+		i++
+	}
+
+	fmt.Println("------------------------------")
 }
 
 func main() {
+	l1 := &ListNode{
+		2,
+		&ListNode{
+			4,
+			&ListNode{
+				3,
+				nil,
+			},
+		},
+	}
 
+	res := rotateRight(l1, 1)
+	printListNodes(res)
+
+	l1 = &ListNode{
+		2,
+		&ListNode{
+			4,
+			&ListNode{
+				3,
+				nil,
+			},
+		},
+	}
+
+	res = rotateRight(l1, 4)
+	printListNodes(res)
 }
