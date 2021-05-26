14f13e96dd8e7206f8a938c6f8a58ad61f7aac3e
calmwu <wubo0067@hotmail.com>
2021-05-26 16:25:31 +0800

    remote Elem()

--- utils/epoll.go
+++ utils/epoll.go
@@ -228,7 +228,7 @@ func NetConnsocketFD(conn net.Conn) int {
 	//tls := reflect.TypeOf(conn.UnderlyingConn()) == reflect.TypeOf(&tls.Conn{})
 	// Extract the file descriptor associated with the connection
 	//connVal := reflect.Indirect(reflect.ValueOf(conn)).FieldByName("conn").Elem()
-	tcpConn := reflect.Indirect(reflect.ValueOf(conn)).FieldByName("conn").Elem()
+	tcpConn := reflect.Indirect(reflect.ValueOf(conn)).FieldByName("conn")
 	//if tls {
 	//	tcpConn = reflect.Indirect(tcpConn.Elem())
 	//}
