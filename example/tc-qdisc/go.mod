34cb11485a5833021c8e15d77d0e181576ea6198
calmwu <wubo0067@hotmail.com>
2021-05-27 12:24:41 +0800

    fix

---
+++ example/tc-qdisc/go.mod
@@ -0,0 +1,5 @@
+module tc.test
+
+go 1.15
+
+require github.com/florianl/go-tc v0.2.0
---
+++ leetcode/addTwoNums/addtwonums.go
@@ -0,0 +1,12 @@
+/*
+ * @Author: CALM.WU
+ * @Date: 2021-03-19 14:48:58
+ * @Last Modified by:   CALM.WU
+ * @Last Modified time: 2021-03-19 14:48:58
+ */
+
+package main
+
+func main() {
+
+}
