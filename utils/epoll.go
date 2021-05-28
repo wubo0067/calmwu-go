c431c80a7ed479924a80abe3389b8d9ff4b4c785
calmwu <wubo0067@hotmail.com>
2021-05-28 14:31:03 +0800

    1：代码合并，修改

--- utils/epoll.go
+++ utils/epoll.go
@@ -120,7 +120,7 @@ func (ep *Epoll) Add(conn, connArg interface{}) (int, error) {
 	}
 
 	// nonblock
-	// unix.SetNonblock(econn.SocketFD, true)
+	unix.SetNonblock(econn.SocketFD, true)
 
 	/*
 		2.6.17 版本内核中增加了 EPOLLRDHUP 事件，代表对端断开连接，关于添加这个事件的理由可以参见 “[Patch][RFC] epoll and half closed TCP connections”。
@@ -170,7 +170,7 @@ func (ep *Epoll) Remove(socketFD int) error {
 	}
 
 	// 判断是否已经注册
-	if _, exist := ep.connections[econn.SocketFD]; ！exist {
+	if _, exist := ep.connections[econn.SocketFD]; !exist {
 		return -1, ErrNotRegistered
 	}
 
