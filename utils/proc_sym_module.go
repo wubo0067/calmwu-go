c21bcabfa7da0bd020ab31ebeaea6c7f28d38b5f
calm.wu <wubo0067@hotmail.com>
2023-09-13 16:59:03 +0800

    feat:implement resolve go pc to symbol

--- utils/proc_sym_module.go
+++ utils/proc_sym_module.go
@@ -2,7 +2,7 @@
  * @Author: CALM.WU
  * @Date: 2023-01-10 14:20:15
  * @Last Modified by: CALM.WU
- * @Last Modified time: 2023-01-10 16:17:22
+ * @Last Modified time: 2023-09-12 18:33:05
  */
 
 package utils
@@ -39,8 +39,8 @@ const (
 )
 
 type ProcSym struct {
-	address uint64
-	name    string
+	pc   uint64
+	name string
 }
 
 type ProcMapPermissions struct {
@@ -57,9 +57,9 @@ type ProcMapPermissions struct {
 }
 
 type ProcSymsModule struct {
-	// StartAddr is the starting address of current mapping.
+	// StartAddr is the starting pc of current mapping.
 	StartAddr uint64
-	// EndAddr is the ending address of current mapping.
+	// EndAddr is the ending pc of current mapping.
 	EndAddr uint64
 	// Perm is the permission of current mapping.
 	Perms ProcMapPermissions
@@ -75,22 +75,48 @@ type ProcSymsModule struct {
 	Type ProcSymModuleType
 	//
 	procSymTable []*ProcSym
+	//
+	goSymTable *GoSymTable
+}
+
+func (psm *ProcSymsModule) open(pid int) (*os.File, *elf.File, error) {
+	// rootfs: /proc/%d/root
+	var (
+		f    *os.File
+		elfF *elf.File
+		err  error
+	)
+	modulePath := fmt.Sprintf("/proc/%d/root%s", pid, psm.Pathname)
+	f, err = os.OpenFile(modulePath, os.O_RDONLY, 0)
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "open module file:'%s'.", modulePath)
+	}
+
+	elfF, err = elf.NewFile(f)
+	if err != nil {
+		f.Close()
+		return nil, nil, errors.Wrap(err, "new ELF file.")
+	}
+
+	return f, elfF, nil
 }
 
 // It reads the contents of /proc/pid/maps, parses each line, and returns a slice of ProcMap entries.
-func (psm *ProcSymsModule) __loadProcModule(pid int) error {
-	//
-	nsRelativePath := fmt.Sprintf("/proc/%d/root%s", pid, psm.Pathname)
+func (psm *ProcSymsModule) loadProcModule(pid int) error {
+	var (
+		f    *os.File
+		elfF *elf.File
+		err  error
+	)
 
 	// 打开elf文件
-	elfFile, err := elf.Open(nsRelativePath)
-	if err != nil {
-		return errors.Wrapf(err, "open elfFile:'%s' failed.", nsRelativePath)
+	if f, elfF, err = psm.open(pid); err != nil {
+		return errors.Wrapf(err, "psm open:'/proc/%d/root%s'.", pid, psm.Pathname)
 	}
-	defer elfFile.Close()
+	defer f.Close()
 
 	// 获取module类型
-	switch elfFile.Type {
+	switch elfF.Type {
 	case elf.ET_EXEC:
 		psm.Type = EXEC
 	case elf.ET_DYN:
@@ -99,26 +125,40 @@ func (psm *ProcSymsModule) __loadProcModule(pid int) error {
 		return ErrProcModuleNotSupport
 	}
 
-	// from .text section read symbol and address
-	symbols, err := elfFile.Symbols()
-	if err != nil {
+	// from .text section read symbol and pc
+	symbols, err := elfF.Symbols()
+	if err != nil && !errors.Is(err, elf.ErrNoSymbols) {
+		return errors.Wrapf(err, "read elfFile:'%s' SYMTAB.", psm.Pathname)
+	}
+
+	dynSymbols, err := elfF.DynamicSymbols()
+	if err != nil && !errors.Is(err, elf.ErrNoSymbols) {
+		return errors.Wrapf(err, "read elfFile:'%s' DYNSYM.", psm.Pathname)
+	}
+
+	total := len(symbols) + len(dynSymbols)
+	if total == 0 {
 		return ErrProcModuleNotSymbolSection
 	}
 
 	for _, symbol := range symbols {
-		if int(symbol.Section) < len(elfFile.Sections) &&
-			elfFile.Sections[symbol.Section].Name == ".text" &&
+		if int(symbol.Section) < len(elfF.Sections) &&
+			elfF.Sections[symbol.Section].Name == ".text" &&
 			len(symbol.Name) > 0 {
 			ps := new(ProcSym)
 			ps.name = symbol.Name
-			ps.address = symbol.Value
+			ps.pc = symbol.Value
+			fmt.Printf("module:'%s' symbol:'%s' pc:'%d'.\n", psm.Pathname, ps.name, ps.pc)
 			psm.procSymTable = append(psm.procSymTable, ps)
 		}
 	}
 
-	// 排序
+	// 按地址排序，地址相同按名字排序
 	sort.Slice(psm.procSymTable, func(i, j int) bool {
-		return psm.procSymTable[i].address < psm.procSymTable[j].address
+		if psm.procSymTable[i].pc == psm.procSymTable[j].pc {
+			return psm.procSymTable[i].name < psm.procSymTable[j].name
+		}
+		return psm.procSymTable[i].pc < psm.procSymTable[j].pc
 	})
 
 	return nil
@@ -130,10 +170,10 @@ func (psm *ProcSymsModule) String() string {
 		psm.StartAddr, psm.EndAddr, psm.Perms, psm.Offset, psm.Dev, psm.Inode, psm.Pathname, len(psm.procSymTable))
 }
 
-func (psm *ProcSymsModule) __resolveAddr(addr uint64) (string, uint32, string, error) {
+func (psm *ProcSymsModule) __resolvePC(pc uint64) (string, uint32, string, error) {
 	// 二分查找
 	index := sort.Search(len(psm.procSymTable), func(i int) bool {
-		return psm.procSymTable[i].address > addr
+		return psm.procSymTable[i].pc > pc
 	})
 
 	// addr小于所有symbol的最小地址
@@ -143,7 +183,7 @@ func (psm *ProcSymsModule) __resolveAddr(addr uint64) (string, uint32, string, e
 
 	// 找到了
 	ps := psm.procSymTable[index-1]
-	return ps.name, uint32(addr - ps.address), psm.Pathname, nil
+	return ps.name, uint32(pc - ps.pc), psm.Pathname, nil
 }
 
 type ProcSyms struct {
@@ -158,20 +198,25 @@ type ProcSyms struct {
 // It parses a line from the /proc/<pid>/maps file and returns a ProcSymsModule struct
 func __parseProcMapEntry(line string, pss *ProcSyms) error {
 	// 7ff8be1a5000-7ff8be1c0000 r-xp 00000000 fd:00 570150                     /usr/lib64/libpthread-2.28.so
+	var (
+		err                error
+		perms              string
+		devMajor, devMinor uint64
+	)
+
 	fields := strings.Fields(line)
 	field_count := len(fields)
 	if field_count != __miniProcMapsEntryDefaultFieldCount {
 		return nil
 	}
 
-	var perms string
-	var devMajor, devMinor uint64
-
 	psm := new(ProcSymsModule)
 	psm.Type = UNKNOWN
 
 	fmt.Sscanf(line, "%x-%x %s %x %x:%x %d %s", &psm.StartAddr, &psm.EndAddr, &perms,
 		&psm.Offset, &devMajor, &devMinor, &psm.Inode, &psm.Pathname)
+
+	fmt.Printf("parse line:'%s'\n", line)
 
 	if len(psm.Pathname) == 0 ||
 		strings.Contains(psm.Pathname, "[vdso]") ||
@@ -201,13 +246,15 @@ func __parseProcMapEntry(line string, pss *ProcSyms) error {
 
 	psm.Dev = unix.Mkdev(uint32(devMajor), uint32(devMinor))
 
-	if err := psm.__loadProcModule(pss.Pid); err != nil {
-		if errors.Is(err, ErrProcModuleNotSupport) ||
-			errors.Is(err, ErrProcModuleNotSymbolSection) {
-			fmt.Printf("module:'%s' load failed. err:%s\n", psm.Pathname, err.Error())
-			return nil
+	// 测试golang程序的load
+	if err = psm.loadProcGoModule(pss.Pid); err != nil {
+		if err = psm.loadProcModule(pss.Pid); err != nil {
+			if errors.Is(err, ErrProcModuleNotSupport) {
+				fmt.Printf("module:'%s' not support read symbols.\n", psm.Pathname)
+				return nil
+			}
+			return errors.Wrapf(err, "load module:'%s' failed.", psm.Pathname)
 		}
-		return errors.Wrapf(err, "load module:'%s' failed.", psm.Pathname)
 	}
 
 	pss.Modules = append(pss.Modules, psm)
@@ -236,6 +283,7 @@ func NewProcSyms(pid int) (*ProcSyms, error) {
 	scanner := bufio.NewScanner(procMapsFile)
 
 	for scanner.Scan() {
+		// maps每一行的信息
 		text := scanner.Text()
 		err := __parseProcMapEntry(text, pss)
 		if err != nil {
@@ -246,20 +294,28 @@ func NewProcSyms(pid int) (*ProcSyms, error) {
 	return pss, nil
 }
 
-// Used to find the symbol of the specified address.
-func (pss *ProcSyms) FindPsym(addr uint64) (string, uint32, string, error) {
+// Used to find the symbol of the specified pc.
+func (pss *ProcSyms) ResolvePC(pc uint64) (string, uint32, string, error) {
 	if len(pss.Modules) == 0 {
 		return "", 0, "", errors.New("proc modules is empty")
 	}
 
 	for _, psm := range pss.Modules {
-		if addr >= psm.StartAddr && addr <= psm.EndAddr {
+		if pc >= psm.StartAddr && pc <= psm.EndAddr {
 			if psm.Type == SO {
-				return psm.__resolveAddr(addr - psm.StartAddr)
+				return psm.__resolvePC(pc - psm.StartAddr)
 			} else if psm.Type == EXEC {
-				return psm.__resolveAddr(addr)
+				fmt.Printf("module:'%s' pc:'%x' is executable.\n", psm.Pathname, pc)
+				if psm.goSymTable != nil {
+					symName, offset, err := psm.goSymTable.__resolveGoPC(pc)
+					if err == nil {
+						return symName, offset, psm.Pathname, nil
+					}
+				} else {
+					return psm.__resolvePC(pc)
+				}
 			}
 		}
 	}
-	return "", 0, "", errors.Errorf("addr:%x not in /proc/%d/maps", addr, pss.Pid)
+	return "", 0, "", errors.Errorf("pc:%x resolution failure", pc)
 }
--- utils/proc_sym_module_test.go
+++ utils/proc_sym_module_test.go
@@ -2,17 +2,92 @@
  * @Author: CALM.WU
  * @Date: 2023-01-10 16:14:02
  * @Last Modified by: CALM.WU
- * @Last Modified time: 2023-01-10 16:17:27
+ * @Last Modified time: 2023-09-12 17:47:28
  */
 
 package utils
 
 import (
+	"debug/elf"
+	"debug/gosym"
+	"encoding/binary"
 	"fmt"
 	"testing"
 )
 
-func TestNewProcSyms(t *testing.T) {
+const (
+	__executable = "/mnt/Program/pyroscope/pyroscope"
+)
+
+func readUint64(data []byte) uint64 {
+	return binary.LittleEndian.Uint64(data)
+}
+
+func readUint32(data []byte) uint32 {
+	return binary.LittleEndian.Uint32(data)
+}
+
+func crack(file string, t *testing.T) (*elf.File, *gosym.Table) {
+	// Open self
+	f, err := elf.Open(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return parse(file, f, t)
+}
+
+func parse(file string, f *elf.File, t *testing.T) (*elf.File, *gosym.Table) {
+	s := f.Section(".gosymtab")
+	if s == nil {
+		t.Skip("no .gosymtab section")
+	}
+	symdat, err := s.Data()
+	if err != nil {
+		f.Close()
+		t.Fatalf("reading %s gosymtab: %v", file, err)
+	}
+	pclndat, err := f.Section(".gopclntab").Data()
+	if err != nil {
+		f.Close()
+		t.Fatalf("reading %s gopclntab: %v", file, err)
+	}
+
+	pcln := gosym.NewLineTable(pclndat, f.Section(".text").Addr)
+	tab, err := gosym.NewTable(symdat, pcln)
+	if err != nil {
+		f.Close()
+		t.Fatalf("parsing %s gosymtab: %v", file, err)
+	}
+
+	return f, tab
+}
+
+// GO111MODULE=off go test -v -run=TestGOSymTab
+func TestGOSymTab(t *testing.T) {
+	f, tab := crack(__executable, t)
+	defer f.Close()
+
+	pc := uint64(0x1004f32)
+	symFunc := tab.PCToFunc(pc)
+	if symFunc != nil {
+		t.Logf("pc:%d ===> func:'%s + %d', entry:%d, end:%d\n",
+			pc, symFunc.Name, symFunc.End-pc, symFunc.Entry, symFunc.End)
+	} else {
+		t.Fatal("pc to func failed")
+	}
+
+	pc = uint64(0x7db549)
+	symFunc = tab.PCToFunc(pc)
+	if symFunc != nil {
+		t.Logf("pc:%d ===> func:'%s + %d', entry:%d, end:%d\n",
+			pc, symFunc.BaseName(), symFunc.End-pc, symFunc.Entry, symFunc.End)
+	} else {
+		t.Fatal("pc to func failed")
+	}
+}
+
+// GO111MODULE=off go test -v -run=TestResolveGO
+func TestResolveGO(t *testing.T) {
 	pid := 4607
 
 	pss, err := NewProcSyms(pid)
@@ -20,18 +95,35 @@ func TestNewProcSyms(t *testing.T) {
 		t.Fatal(err.Error())
 	}
 
-	fmt.Printf("pid:'%d' module count:%d", pid, len(pss.Modules))
+	pcList := []uint64{
+		0x1004f32,
+		0x47f067,
+		0x1a8e40d,
+		0x1a8b052,
+		0x1a991d8,
+	}
 
-	for i, psm := range pss.Modules {
-		fmt.Printf("[%d] module: %s\n", i, psm.String())
-
-		for j, ps := range psm.procSymTable {
-			fmt.Printf("\t[%d] 0x%x\t%s\n", j, ps.address, ps.name)
+	for _, pc := range pcList {
+		name, offset, moduleName, err := pss.ResolvePC(pc)
+		if err == nil {
+			t.Logf("addr:0x%x %s+0x%02x [%s]\n\n", pc, name, offset, moduleName)
+		} else {
+			t.Fatal(err.Error())
 		}
+	}
+}
+
+// GO111MODULE=off go test -v -run=TestResolveCApp
+func TestResolveCApp(t *testing.T) {
+	pid := 4607
+
+	pss, err := NewProcSyms(pid)
+	if err != nil {
+		t.Fatal(err.Error())
 	}
 
 	addr := uint64(0x00007fa9984962a6)
-	name, offset, moduleName, err := pss.FindPsym(addr)
+	name, offset, moduleName, err := pss.ResolvePC(addr)
 	if err != nil {
 		t.Fatal(err.Error())
 	} else {
@@ -39,7 +131,7 @@ func TestNewProcSyms(t *testing.T) {
 	}
 
 	addr = uint64(0x00007fa998495b47)
-	name, offset, moduleName, err = pss.FindPsym(addr)
+	name, offset, moduleName, err = pss.ResolvePC(addr)
 	if err != nil {
 		t.Fatal(err.Error())
 	} else {
@@ -47,7 +139,7 @@ func TestNewProcSyms(t *testing.T) {
 	}
 
 	addr = uint64(0x00007fa99848c1df)
-	name, offset, moduleName, err = pss.FindPsym(addr)
+	name, offset, moduleName, err = pss.ResolvePC(addr)
 	if err != nil {
 		t.Fatal(err.Error())
 	} else {
@@ -55,7 +147,7 @@ func TestNewProcSyms(t *testing.T) {
 	}
 
 	addr = uint64(0x41f3b3)
-	name, offset, moduleName, err = pss.FindPsym(addr)
+	name, offset, moduleName, err = pss.ResolvePC(addr)
 	if err != nil {
 		t.Fatal(err.Error())
 	} else {
